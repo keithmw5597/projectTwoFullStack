0cc47e13cf9c1a4aa0242245b1cd5d2813d8c0b6
drkevinfriday <kevinfriday87@gmail,com>
2022-06-27 18:14:04 -0500

    file structure and  middleware update

---
+++ controller/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const departmentRoutes = require('./department-routes');
+const employeeRoutes = require('./employee-routes')
+const salaryRoutes = require('./salary-routes')
+
+router.use('/departments', departmentRoutes);
+router.use('/employees', employeeRoutes)
+router.use('/salarys', salaryRoutes)
+
+module.exports = router;
