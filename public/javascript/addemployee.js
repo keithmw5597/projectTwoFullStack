7c9203f7de83d4e2c7267eaebc81b95fd461c518
keithmw <keithmw5597@yahoo.com>
2022-07-03 11:50:42 -0500

    add employee

--- public/javascript/addemployee.js
+++ public/javascript/addemployee.js
@@ -3,14 +3,14 @@ const { sequelize } = require("../../models/Employees");
 
 const sequelize = require(sequelize)
 const email = document.querySelector('#email').value.trim();
-const firstName = document.querySelector('#first name').value.trim();
-const lastName = document.querySelector('last name').value.trim();
-const phoneNumber = document.querySelector('phone').value.trim();
-const dob = document.querySelector('date of birth').value.trim();
-const role = document.querySelector('role').value.trim();
-const department = document.querySelector('department').value.trim();
-const employeeId = document.querySelector('employeeId').value.trim();
-const manager = document.querySelector('manager').value.trim();
+const firstName = document.querySelector('#fName').value.trim();
+const lastName = document.querySelector('#lName').value.trim();
+const phoneNumber = document.querySelector('#phone').value.trim();
+const dob = document.querySelector('#DOB').value.trim();
+const role = document.querySelector('#role').value.trim();
+const department = document.querySelector('#department').value.trim();
+const employeeId = document.querySelector('#employeeId').value.trim();
+const manager = document.querySelector('#manager').value.trim();
 
    
 async function addemployeeHandler(event) {
